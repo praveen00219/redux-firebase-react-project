f4002f04b61ff026d8ea75aa41cb03e5e08e4b41
praveen00219 <paru2192000@gmail.com>
2024-10-27 00:34:18 +0530

    removed margin

--- src/components/favContactsTableList/FavContactDataListing.jsx
+++ src/components/favContactsTableList/FavContactDataListing.jsx
@@ -1,6 +1,4 @@
 import React from "react";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-
 import Profile from "../../assets/All Contact.png";
 
 function FavContactDataListing({ favContacts }) {
